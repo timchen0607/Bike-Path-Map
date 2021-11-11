948d53fdc90d9291948d734360345bec7f40c40c
Tim School <a0963573232@gmail.com>
2021-11-11 15:09:34 +0800

    Fix: 更新 API Key

--- src/modules.js
+++ src/modules.js
@@ -1,8 +1,8 @@
 // API 驗證
 import jsSHA from "jssha/dist/sha1";
 const getAuthHeader = () => {
-  const AppID = "2dfbcb68d0e04fb5bc9d1f59f2e3df35";
-  const AppKey = "HmEvi7uEhV77UNGt-MYo6ETs43E";
+  const AppID = process.env.VUE_APP_PTX_ID;
+  const AppKey = process.env.VUE_APP_PTX_KEY;
   const GMTString = new Date().toGMTString();
   const ShaObj = new jsSHA("SHA-1", "TEXT");
   ShaObj.setHMACKey(AppKey, "TEXT");
@@ -12,26 +12,31 @@ const getAuthHeader = () => {
   return { Authorization: Authorization, "X-Date": GMTString };
 };
 
+const perPage = 18; // 每頁顯示筆數
+// mode => ScenicSpot/Restaurant/Hotel/Activity
+// $count=true 查看 API 剩餘次數
+
 // 抓取 自行車租借站/及時車位 相關資料
 // mode => Station/Availability
-const getBikeInfo = (mode, city, skip = 0) => {
-  let url = "https://ptx.transportdata.tw/MOTC/v2/Bike/";
-  url += `${mode}/${city}?$top=20&$skip=${skip}&$format=JSON`;
+const getBikeInfo = (mode, city, page = 1) => {
+  let url = `https://ptx.transportdata.tw/MOTC/v2/Bike/${mode}/${city}?`;
+  url += `$top=${perPage}&$skip=${(page - 1) * perPage}&$format=JSON`;
   return fetch(url, { headers: getAuthHeader() }).then((res) => res.json());
 };
 
 // 抓取 自行車道圖資
-const getBikeShape = (city, skip = 0) => {
-  let url = "https://ptx.transportdata.tw/MOTC/v2/Cycling/Shape/";
-  url += `${city}?$top=20&$skip=${skip}&$format=JSON`;
+const getBikeShape = (city, page = 1) => {
+  let url = `https://ptx.transportdata.tw/MOTC/v2/Cycling/Shape/${city}?`;
+  url += `$top=${perPage}&$skip=${(page - 1) * perPage}&$format=JSON`;
   return fetch(url, { headers: getAuthHeader() }).then((res) => res.json());
 };
 
 // 抓取 自行車租借站/及時車位 鄰近相關資料
 // mode => Station/Availability
-const getNearbyBikeInfo = (mode, city, lat, lon, meters = 1000, skip = 0) => {
-  let url = "https://ptx.transportdata.tw/MOTC/v2/Bike/";
-  url += `${mode}/${city}?$top=20&$skip=${skip}&$spatialFilter=nearby(${lat}%2C${lon}%2C${meters})&$format=JSON`;
+const getNearbyBikeInfo = (mode, city, lat, lon, meters = 1000, page = 1) => {
+  let url = `https://ptx.transportdata.tw/MOTC/v2/Bike/${mode}/${city}?`;
+  url += `$top=${perPage}&$skip=${(page - 1) * perPage}`;
+  url += `&$spatialFilter=nearby(${lat}%2C${lon}%2C${meters})&$format=JSON`;
   return fetch(url, { headers: getAuthHeader() }).then((res) => res.json());
 };
 
